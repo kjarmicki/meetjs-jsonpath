cbece9f239d1fe39a4f9cbd56e44c24b770bcf99
kjarmicki <jarmicki@gmail.com>
2016-02-11 21:19:39 +0100

    init

---
+++ benchmark/benchmark.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const transform = require('jsonpath-object-transform'),
+    volumes = require('./volumes'),
+    assert = require('assert'),
+    microtime = require('microtime');
+
+function jsonPathBooksInfo(volumes) {
+    const template = {
+        items: ['$..items', {
+            authors: '$..authors',
+            title: '$..title',
+            isbn13: '$..industryIdentifiers..[?(@.type === "ISBN_13")].identifier'
+        }]
+    };
+
+    return transform(volumes, template).items;
+}
+
+function nativeJsBooksInfo(volumes) {
+    return volumes.items.map(volume => {
+        let info = {
+            authors: undefined,
+            title: undefined,
+            isbn13: undefined
+        };
+
+        try {
+            info.authors = volume.volumeInfo.authors;
+        } catch(e) {}
+        try {
+            info.title = volume.volumeInfo.title;
+        } catch(e) {}
+        try {
+            info.isbn13 = volume.volumeInfo.industryIdentifiers
+                .filter(id => id.type === 'ISBN_13')[0]
+                .identifier;
+        } catch(e) {}
+
+        return info;
+    });
+}
+
+// to make sure benchmark methods are fair
+assert.deepEqual(jsonPathBooksInfo(volumes), nativeJsBooksInfo(volumes));
+
+const repeats = 100;
+
+const nativeStart = microtime.now();
+for(let i = 0; i < repeats; i++) {
+    nativeJsBooksInfo(volumes);
+}
+const nativeEnd = microtime.now();
+
+
+const jsonPathStart = microtime.now();
+for(let i = 0; i < repeats; i++) {
+    jsonPathBooksInfo(volumes);
+}
+const jsonPathEnd = microtime.now();
+
+
+console.log(`
+*** RESULTS ***
+Native JS:  ${(nativeEnd - nativeStart) / 1000}ms
+JSON Path:  ${(jsonPathEnd - jsonPathStart) / 1000}ms
+
+Oh noes, JSON Path is about ${Math.round((jsonPathEnd - jsonPathStart) / (nativeEnd - nativeStart))} times slower
+`);
